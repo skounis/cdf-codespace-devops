a91336c81633bab07016f154b6398fbca837bbfe
Stavros Kounis <skounis@gmail.com>
2023-01-09 05:50:56 +0000

    Improve cli. Add documentation.

--- README.md
+++ README.md
@@ -7,3 +7,21 @@ virtualenv ~/.codespace
 source ~/.codespace/bin/activate
 which python
 ```
+
+## Setup the CLI
+
+Make the `.py` file executable
+```bash
+chmod +x hello.py
+```
+
+Run with help
+```bash
+./hello.py --help
+```
+
+Example 
+```bash
+./hello.py --path . --ftype py
+./hello.py --path . --ftype txt
+```
--- hello.py
+++ hello.py
@@ -1,2 +1,29 @@
-def Hi():
-    return "hi"
+#!/usr/bin/env python
+import glob
+import click
+
+# this is bad code intentionally
+# varbad=
+
+
+@click.command()
+@click.option(
+    "--path",
+    prompt="Path to search for csv files",
+    help="This is the path to search for files: /tmp",
+)
+@click.option(
+    "--ftype", prompt="Pass in the type of file", help="Pass in the file type:  i.e csv"
+)
+def search(path, ftype):
+    """This is a tool that search for patterns like *.csv"""
+
+    results = glob.glob(f"{path}/*.{ftype}")
+    click.echo(click.style("Found Matches:", fg="red"))
+    for result in results:
+        click.echo(click.style(f"{result}", bg="yellow", fg="white"))
+
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    search()
